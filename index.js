4d1a5fa27e0dadab97991a6dbe57356821a7112c
babatunde <babsodedina@gmail.com>
2023-01-07 10:42:08 +0100

    corrections made by babatunde

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-const totalEl = document.getElementById("total");
-const remainingEl = document.getElementById("remaining");
-// const textEl = document.getElementById("text");
-const textEl= document.querySelector("textarea")
+const totalEl = document.getElementById('total');
+const remainingEl = document.getElementById('remaining');
+const textEl = document.querySelector('.textarea');
+const maxLength = 60;
 
-textEl.addEventListener("keyup", () => {
+textEl.addEventListener('keyup', () => {
   updateCounter();
 });
 
 function updateCounter() {
+  const maxLength = textEl.getAttribute('maxlength');
   totalEl.innerHTML = textEl.value.length;
-  remainingEl.innerHTML =
-     textEl.getAttribute("maxlenght")
-    // console.log(textEl.getAttribute("maxlenght"))
-
-    const maxLenght = textEl.getAttribute("maxlenght")
-    console.log(maxLenght)
-
+  remainingEl.innerHTML = maxLength - textEl.value.length;
 }
 
-updateCounter()
+updateCounter();
